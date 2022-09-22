6fdfc41596d1de82dfb7facdf591987ff38d86b6
Yariksas <bryuh.yaroslaw@gmail.com>
2022-09-23 01:34:38 +0200

    Homework No.3

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ви ввели число ");
+            Console.Write(i);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+            string message = "Ви ввели число " + Convert.ToString(i);
+            string caption = "Solution";
+            MessageBox.Show(message, caption);
+        }
+    }
+}
